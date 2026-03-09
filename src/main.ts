a011eb893abd9254b4ade3c4d7189074132dda58
rakhzar <mnv36ru@gmail.com>
2026-03-09 19:01:31 +0300

    feat: инициализация Pinia, добавлен profile.store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/main.css'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.mount('#app');
