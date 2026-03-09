e7fdbbf4c7ead83c09f4f839ef9e3c04771f000b
rakhzar <mnv36ru@gmail.com>
2026-03-09 11:41:19 +0300

    feat: initial project setup with base styles and config

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <nav class="nav">Меню</nav>
+    <main>Контент</main>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140);
+  gap: 200px;
+  max-width: 1450px;
+  margin: 140px auto 0 auto;
+}
+
+.nav {
+  min-width: 400px;
+}
+</style>
