3c55b47a37c06bf5c66d54fd2c8a969a565703f8
rakhzar <mnv36ru@gmail.com>
2026-03-10 13:56:12 +0300

    feat: добавлен путь /categories в API_ROUTES

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-export const API_ROUTES = {
-  profile: `/profile`,
-};
-
 export const http = axios.create({
   baseURL: 'http://localhost:3000/api',
   timeout: 10000,
 });
+
+export const API_ROUTES = {
+  profile: `/profile`,
+  categories: `/categories`,
+};
