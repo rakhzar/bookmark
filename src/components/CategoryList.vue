f8a1eae793a5da0c92b8de37f010e313506f7c5e
rakhzar <mnv36ru@gmail.com>
2026-03-10 14:18:58 +0300

    feat: CategoriesList с динамическими ссылками

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useCategoryStore } from '@/stores/categories.store';
+import { onMounted } from 'vue';
+
+const store = useCategoryStore();
+
+onMounted(() => {
+  store.fetchCategories();
+});
+</script>
+
+<template>
+  <ul class="category-list">
+    <li
+      class="list-item"
+      v-for="item in store.categories"
+      :key="item.id"
+    >
+      <a :href="`/${item.alias}`">{{ item.name }}</a>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.category-list {
+  display: flex;
+  flex-direction: column;
+  gap: 34px;
+  margin: 0;
+  padding: 0;
+}
+
+.list-item {
+  list-style: none;
+}
+
+.list-item a {
+  text-decoration: none;
+  transition: all 0.5s;
+  color: var(--color-fg);
+}
+
+.list-item a:hover {
+  font-size: 24px;
+  font-weight: 700;
+}
+</style>
