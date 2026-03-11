74d5948c45e8ca5eb1b7dd5f81007835f59e2a38
rakhzar <mnv36ru@gmail.com>
2026-03-11 12:28:04 +0300

    refactor: вынос логики из App.vue в MainView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,3 @@
-<script setup lang="ts">
-import CategoryList from './components/CategoryList.vue';
-import ProfileAvatar from './components/ProfileAvatar.vue';
-</script>
-
 <template>
-  <div class="app">
-    <nav class="nav">
-      <ProfileAvatar />
-      <CategoryList />
-    </nav>
-    <main>
-      Контент
-      <TestProfile />
-    </main>
-  </div>
+  <RouterView />
 </template>
-
-<style scoped>
-.app {
-  display: flex;
-  min-height: calc(100vh - 140);
-  gap: 200px;
-  max-width: 1450px;
-  margin: 140px auto 0 auto;
-}
-
-.nav {
-  min-width: 400px;
-}
-</style>
