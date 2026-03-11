20e4e01fee73b07eb296dff2553b3638c9de9f7b
rakhzar <mnv36ru@gmail.com>
2026-03-11 12:29:11 +0300

    feat: добавлена AuthView с заголовком и кнопкой входа

---
+++ src/views/AuthView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import ButtonText from '@/components/ButtonText.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <div class="auth">
+    <div class="auth-form">
+      <h1 class="auth__header">Bookmarkly</h1>
+      <ButtonText @click="router.push('/main')"
+        >Вход
+      </ButtonText>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.auth {
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+}
+
+.auth__header {
+  font-size: 52px;
+  font-weight: 700;
+}
+
+.auth-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 52px;
+}
+</style>
