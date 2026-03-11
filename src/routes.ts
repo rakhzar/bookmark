8ba2788ed48099f09f856c0f65e2bf51d393f5a6
rakhzar <mnv36ru@gmail.com>
2026-03-11 14:00:54 +0300

    refactor: routes — подключены через динамический import (lazy loading)

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: () => import('./views/AuthView.vue'),
+    },
+    {
+      path: '/main',
+      component: () => import('./views/MainView.vue'),
+      children: [
+        {
+          path: '',
+          component: () => import('./components/TestComp.vue'),
+          name: 'main',
+        },
+        {
+          path: 'new',
+          component: () => import('./components/TestComp2.vue'),
+        },
+      ],
+    },
+  ],
   history: createWebHistory(),
 });
