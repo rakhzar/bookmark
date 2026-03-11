78e17b229ce2577d12836ac352d759e959fc24b1
rakhzar <mnv36ru@gmail.com>
2026-03-11 10:12:48 +0300

    feat: добавлен vue-router, базовая конфигурация

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  routes: [],
+  history: createWebHistory(),
+});
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
+app.use(router);
 app.mount('#app');
