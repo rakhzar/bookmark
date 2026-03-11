c4ea92d57005d353809e21cfd747df4be5ce04f3
rakhzar <mnv36ru@gmail.com>
2026-03-11 13:58:15 +0300

    refactor: categories.store — убран мусор, приведён к единому стилю

--- src/stores/categories.store.ts
+++ src/stores/categories.store.ts
@@ -3,17 +3,13 @@ import type { Category } from '@/interfaces/category.interface';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-export const useCategoryStore = defineStore(
-  'categories',
-  () => {
-    const categories = ref<Category[]>([]);
-    async function fetchCategories() {
-      const { data } = await http.get<Category[]>(
-        API_ROUTES.categories,
-      );
-      categories.value = data;
-    }
+export const useCategoryStore = defineStore('categories', () => {
+  const categories = ref<Category[]>([]);
 
-    return { categories, fetchCategories };
-  },
-);
+  async function fetchCategories() {
+    const { data } = await http.get<Category[]>(API_ROUTES.categories);
+    categories.value = data;
+  }
+
+  return { categories, fetchCategories };
+});
