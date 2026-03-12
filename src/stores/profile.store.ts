dab4b3226797254332b62146e28328178729f4c3
rakhzar <mnv36ru@gmail.com>
2026-03-12 15:29:44 +0300

    refactor: использование client() вместо прямого http в profile.store

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -1,20 +1,15 @@
-import { API_ROUTES, http } from '@/api';
+import { API_ROUTES, client } from '@/api';
 import type { Profile } from '@/interfaces/profile.interface';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-export const useProfileStore = defineStore(
-  'profile',
-  () => {
-    const profile = ref<Profile>();
+export const useProfileStore = defineStore('profile', () => {
+  const profile = ref<Profile>();
 
-    async function fetchProfile() {
-      const { data } = await http.get<Profile>(
-        API_ROUTES.profile,
-      );
-      profile.value = data;
-    }
+  async function fetchProfile() {
+    const { data } = await client().get<Profile>(API_ROUTES.profile);
+    profile.value = data;
+  }
 
-    return { profile, fetchProfile };
-  },
-);
+  return { profile, fetchProfile };
+});
