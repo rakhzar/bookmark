fbc0abaaa669b4ee8f301cf2b9022a691b727db3
rakhzar <mnv36ru@gmail.com>
2026-03-12 11:47:35 +0300

    feat: IndexView — разметка и стили для главной страницы

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,1 +1,28 @@
-<template>Index</template>
+<template>
+  <div class="index">
+    <h1 class="index__header">Добро пожаловать в Bookmarkly</h1>
+    <p class="index__description">
+      Чтобы начать пользоваться выберите категорию закладок или создайте новую.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+  min-height: 50vh;
+}
+
+.index__header {
+  font-style: 24px;
+  font-weight: 500;
+}
+
+.index__description {
+  font-style: 16px;
+}
+</style>
