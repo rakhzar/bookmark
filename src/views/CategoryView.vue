e6a00ad7e576115105b30d88d1aa800c9be5194e
rakhzar <mnv36ru@gmail.com>
2026-03-12 20:38:00 +0300

    feat: onMounted в CategoryView получает категорию по alias и загружает закладки

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -2,13 +2,20 @@
 import type { Category } from '@/interfaces/category.interface';
 import { useBookmarkStore } from '@/stores/bookmark.store';
 import { useCategoryStore } from '@/stores/categories.store';
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const categoryStore = useCategoryStore();
 const bookmarkStore = useBookmarkStore();
 const category = ref<Category>();
+
+onMounted(() => {
+  category.value = categoryStore.getCategoryByAlias(route.params.alias);
+  if (category.value) {
+    bookmarkStore.fetchBookmarks(category.value.id);
+  }
+});
 
 watch(
   () => ({
