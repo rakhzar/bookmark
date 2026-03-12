5b1de33538adba4b6ff5cd6e17109ab0ac095f8e
rakhzar <mnv36ru@gmail.com>
2026-03-12 11:38:18 +0300

    feat: созданы CategoryView и IndexView

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+// console.log(route.params.alias);
+</script>
+
+<template>
+  Category
+  {{ $route.params.alias }}
+</template>
