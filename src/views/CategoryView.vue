29be2120ad17e0ac544e2936ad291d6297f9f5d9
rakhzar <mnv36ru@gmail.com>
2026-03-12 20:08:39 +0300

    feat: CategoryView — загрузка закладок при смене категории

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import type { Category } from '@/interfaces/category.interface';
+import { useBookmarkStore } from '@/stores/bookmark.store';
 import { useCategoryStore } from '@/stores/categories.store';
 import { ref, watch } from 'vue';
-import { useRoute, onBeforeRouteUpdate } from 'vue-router';
+import { useRoute } from 'vue-router';
 
 const route = useRoute();
-const state = useCategoryStore();
+const categoryStore = useCategoryStore();
+const bookmarkStore = useBookmarkStore();
 const category = ref<Category>();
 
 watch(
-  () => state.categories,
-  () => {
-    category.value = state.getCategoryByAlias(route.params.alias);
+  () => ({
+    alias: route.params.alias,
+    categories: categoryStore.categories,
+  }),
+  async (data) => {
+    category.value = categoryStore.getCategoryByAlias(data.alias);
+    if (category.value) {
+      bookmarkStore.fetchBookmarks(category.value.id);
+    }
   },
 );
-
-onBeforeRouteUpdate((to) => {
-  category.value = state.getCategoryByAlias(to.params.alias);
-});
 </script>
 
 <template>
   Category
   {{ category?.name }}
+  {{ bookmarkStore.bookmarks.length }}
 </template>
