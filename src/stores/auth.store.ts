d6ff1c1fd724604411f15b3c75d439efe4b53ad0
rakhzar <mnv36ru@gmail.com>
2026-03-13 14:29:55 +0300

    feat: auth — инициализация токена из localStorage

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -8,6 +8,12 @@ const TOKEN_STORE_KEY = 'auth-store';
 export const useAuthStore = defineStore('auth', () => {
   const token = ref<string>();
 
+  const initialValue =
+    localStorage.getItem(TOKEN_STORE_KEY);
+  if (initialValue) {
+    token.value = initialValue;
+  }
+
   function setToken(newToken: string) {
     token.value = newToken;
     localStorage.setItem(TOKEN_STORE_KEY, newToken);
