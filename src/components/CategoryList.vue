73bea531c021c3a2d751515686947ce3c9cfb313
rakhzar <mnv36ru@gmail.com>
2026-03-13 19:53:51 +0300

    feat: добавлен logout — очистка токена и редирект на auth

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -3,26 +3,43 @@ import { useCategoryStore } from '@/stores/categories.store';
 import { onMounted } from 'vue';
 import ButtonIcon from './ButtonIcon.vue';
 import IconPlus from '@/icons/IconPlus.vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth.store';
 
 const store = useCategoryStore();
+const authStore = useAuthStore();
+const router = useRouter();
 
 onMounted(() => {
   store.fetchCategories();
 });
+
+function lagout() {
+  authStore.clearToken();
+  router.push({ name: 'auth' });
+}
 </script>
 
 <template>
   <ul class="category-list">
-    <li class="list-item" v-for="item in store.categories" :key="item.id">
-      <RouterLink active-class="active-link" :to="`/main/${item.alias}`">{{
-        item.name
-      }}</RouterLink>
+    <li
+      class="list-item"
+      v-for="item in store.categories"
+      :key="item.id"
+    >
+      <RouterLink
+        active-class="active-link"
+        :to="`/main/${item.alias}`"
+        >{{ item.name }}</RouterLink
+      >
     </li>
     <li>
       <ButtonIcon @click="store.createCategory">
         <IconPlus />
       </ButtonIcon>
+    </li>
+    <li class="list-item">
+      <a href="#" @click="lagout()">Выход</a>
     </li>
   </ul>
 </template>
