2ae9bfb868367b4233a0e88e4e4409257186589f
rakhzar <mnv36ru@gmail.com>
2026-03-13 21:16:24 +0300

    refactor: categories.store — исправлен синтаксис и логика создания категории

--- src/stores/categories.store.ts
+++ src/stores/categories.store.ts
@@ -4,28 +4,45 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
 
-export const useCategoryStore = defineStore('categories', () => {
-  const categories = ref<Category[]>([]);
+export const useCategoryStore = defineStore(
+  'categories',
+  () => {
+    const categories = ref<Category[]>([]);
 
-  async function fetchCategories() {
-    const { data } = await client().get<Category[]>(API_ROUTES.categories);
-    categories.value = data;
-  }
-
-  async function createCategory() {
-    const { data } = await client().post<Category>(API_ROUTES.categories, {
-      name: 'Новая категория',
-      alias: uuidv4(),
-    });
-    categories.value.push(data);
-  }
-
-  function getCategoryByAlias(alias: string | string[]): Category | undefined {
-    if (typeof alias == 'string') {
-      return categories.value.find((c) => c.alias == alias);
+    async function fetchCategories() {
+      const { data } = await client().get<Category[]>(
+        API_ROUTES.categories,
+      );
+      categories.value = data;
     }
-    return;
-  }
 
-  return { categories, fetchCategories, createCategory, getCategoryByAlias };
-});
+    async function createCategory() {
+      const { data } = await client().post<Category>(
+        API_ROUTES.categories,
+        {
+          name: 'Новая категория',
+          alias: uuidv4(),
+        },
+      );
+      categories.value.push(data);
+    }
+
+    function getCategoryByAlias(
+      alias: string | string[],
+    ): Category | undefined {
+      if (typeof alias == 'string') {
+        return categories.value.find(
+          (c) => c.alias == alias,
+        );
+      }
+      return;
+    }
+
+    return {
+      categories,
+      fetchCategories,
+      createCategory,
+      getCategoryByAlias,
+    };
+  },
+);
