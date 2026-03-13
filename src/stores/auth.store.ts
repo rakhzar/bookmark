1d0d512bec727668edbadf26b9f905d2cce1275f
rakhzar <mnv36ru@gmail.com>
2026-03-13 14:13:50 +0300

    feat: добавлено получение и хранение токена в localStorage

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,10 +1,19 @@
 import { API_ROUTES, client } from '@/api';
 import type { LoginResponse } from '@/interfaces/auth.interface';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+
+const TOKEN_STORE_KEY = 'auth-store';
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref<string>();
+
+  function setToken(newToken: string) {
+    token.value = newToken;
+    localStorage.setItem(TOKEN_STORE_KEY, newToken);
+  }
+
+  const getToken = computed(() => token.value);
 
   async function login(email: string, password: string) {
     const { data } = await client().post<LoginResponse>(
@@ -14,8 +23,8 @@ export const useAuthStore = defineStore('auth', () => {
         password,
       },
     );
-    token.value = data.token;
+    setToken(data.token);
   }
 
-  return { token, login };
+  return { getToken, login, setToken };
 });
