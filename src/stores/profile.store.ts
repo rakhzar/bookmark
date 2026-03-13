35d0fd7bcf8a716e866450b5b15bc60f7a762c31
rakhzar <mnv36ru@gmail.com>
2026-03-13 21:14:56 +0300

    fix: profile.store — исправлен путь на API_ROUTES.auth.profile

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -3,13 +3,18 @@ import type { Profile } from '@/interfaces/profile.interface';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-export const useProfileStore = defineStore('profile', () => {
-  const profile = ref<Profile>();
+export const useProfileStore = defineStore(
+  'profile',
+  () => {
+    const profile = ref<Profile>();
 
-  async function fetchProfile() {
-    const { data } = await client().get<Profile>(API_ROUTES.profile);
-    profile.value = data;
-  }
+    async function fetchProfile() {
+      const { data } = await client().get<Profile>(
+        API_ROUTES.auth.profile,
+      );
+      profile.value = data;
+    }
 
-  return { profile, fetchProfile };
-});
+    return { profile, fetchProfile };
+  },
+);
