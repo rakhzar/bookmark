483f9d6e659e64aac5989030d1ad25407b30f9bf
rakhzar <mnv36ru@gmail.com>
2026-03-13 09:02:32 +0300

    feat: добавлен InputString с defineModel и базовыми стилями

---
+++ src/components/InputString.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const data = defineModel<string>();
+</script>
+
+<template>
+  <input class="input-string" v-model="data" />
+</template>
+
+<style scoped>
+.input-string {
+  border: none;
+  border-bottom: 1px solid var(--color-fg);
+  font-size: 24px;
+  color: var(--color-fg);
+  min-width: 274px;
+}
+
+.input-string::placeholder {
+  color: var(--color-inactive);
+}
+</style>
