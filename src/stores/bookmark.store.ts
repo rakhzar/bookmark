139bc03f696c6e92dc3f1d88f8b837817314461d
rakhzar <mnv36ru@gmail.com>
2026-03-14 17:51:04 +0300

    refactor: использование localStorage для сохранения закладок, чистка кода

--- src/stores/bookmark.store.ts
+++ src/stores/bookmark.store.ts
@@ -1,32 +1,50 @@
-import { API_ROUTES, client } from '@/api';
 import type { Bookmark } from '@/interfaces/bookmark.interface';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+const STORAGE_KEY = 'bookmarks';
+
 export const useBookmarkStore = defineStore(
   'bookmarks',
   () => {
-    const bookmarks = ref<Bookmark[]>([
-      {
-        id: 1,
-        category_id: 2,
-        title:
-          'Github - gofiber/fiber: ⚡ Express inspired webframework written in Go',
-        url: 'https://gofiber.io/',
-        image:
-          'https://repository-images.githubusercontent.com/234231371/00fd8700-5430-11ea-820b-15fd85b2472c',
-        create_at: new Date(),
-      },
-    ]);
+    const saved = localStorage.getItem(STORAGE_KEY);
+    const bookmarks = ref<Bookmark[]>(
+      saved
+        ? JSON.parse(saved)
+        : [
+            {
+              id: 2,
+              category_id: 2,
+              title:
+                'Github - gofiber/fiber: ⚡ Express inspired webframework written in Go',
+              url: 'https://gofiber.io/',
+              image:
+                'https://repository-images.githubusercontent.com/234231371/00fd8700-5430-11ea-820b-15fd85b2472c',
+              create_at: new Date(),
+            },
+            {
+              id: 1,
+              category_id: 2,
+              title:
+                'PurpleSchoo - Обучающая платформа для старта карьеры в IT и роста | Антон Ларечев',
+              url: 'https://purpleschoo.ru/',
+              image:
+                'https://habrastorage.org/getpro/moikrug/uploads/company/100/008/877/4/logo/big_ae43eaf1ccdde19716aff31009e04640.png',
+              create_at: new Date(),
+            },
+          ],
+    );
 
-    async function fetchBookmarks(category_id: number) {
-      const { data } = await client().get<Bookmark[]>(
-        API_ROUTES.bookmarks(category_id),
+    function deleteBookmark(id: number) {
+      bookmarks.value = bookmarks.value.filter(
+        (b) => b.id !== id,
       );
-      if (data.length === 0) return;
-      bookmarks.value = data;
+      localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(bookmarks.value),
+      );
     }
 
-    return { bookmarks, fetchBookmarks };
+    return { bookmarks, deleteBookmark };
   },
 );
