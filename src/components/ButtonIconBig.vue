3c2475c9f66cfbfebefe2db1b337143f698d0555
rakhzar <mnv36ru@gmail.com>
2026-03-14 12:52:42 +0300

    feat: добавлен ButtonIconBig — крупная кнопка-иконка с обводкой

---
+++ src/components/ButtonIconBig.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="button-icon__big">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button-icon__big {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  cursor: pointer;
+  border-radius: 50px;
+  border: 3px solid var(--color-bg);
+  width: 48px;
+  height: 48px;
+}
+.button-icon:hover {
+  background: var(--color-bg-hover);
+}
+</style>
