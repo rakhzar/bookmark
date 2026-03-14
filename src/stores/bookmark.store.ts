b8780e1a682b2eea12ef6d8a993e873b3ec3e65f
rakhzar <mnv36ru@gmail.com>
2026-03-14 15:04:05 +0300

    fix: в fetchBookmarks добавлена проверка на пустой массив, чтобы не терять тестовые закладки

--- src/stores/bookmark.store.ts
+++ src/stores/bookmark.store.ts
@@ -6,12 +6,24 @@ import { ref } from 'vue';
 export const useBookmarkStore = defineStore(
   'bookmarks',
   () => {
-    const bookmarks = ref<Bookmark[]>([]);
+    const bookmarks = ref<Bookmark[]>([
+      {
+        id: 1,
+        category_id: 2,
+        title:
+          'Github - gofiber/fiber: ⚡ Express inspired webframework written in Go',
+        url: 'https://gofiber.io/',
+        image:
+          'https://repository-images.githubusercontent.com/234231371/00fd8700-5430-11ea-820b-15fd85b2472c',
+        create_at: new Date(),
+      },
+    ]);
 
     async function fetchBookmarks(category_id: number) {
       const { data } = await client().get<Bookmark[]>(
         API_ROUTES.bookmarks(category_id),
       );
+      if (data.length === 0) return;
       bookmarks.value = data;
     }
 
