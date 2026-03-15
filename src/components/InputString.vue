23a6789e574b014464d0b80a1accd800b47cea2f
rakhzar <mnv36ru@gmail.com>
2026-03-15 13:59:40 +0300

    feat: InputString — добавлен программный фокус через useTemplateRef

--- src/components/InputString.vue
+++ src/components/InputString.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue';
+
 const data = defineModel<string>();
+const input = useTemplateRef<HTMLInputElement>('input');
+
+const { isFocused = false } = defineProps<{
+  isFocused: boolean;
+}>();
+
+onMounted(() => {
+  if (isFocused && input.value) {
+    input.value.focus();
+  }
+});
 </script>
 
 <template>
-  <input class="input-string" v-model="data" />
+  <input class="input-string" v-model="data" ref="input" />
 </template>
 
 <style scoped>
